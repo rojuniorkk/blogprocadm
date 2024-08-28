c93701e77835931912dc9a5c395288cf8ee9333e
Rogério <ro.juniorkk@gmail.com>
2024-08-28 20:14:19 -0300

    #

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.templates.website')
+
+
+@section('content')
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+
+        <!-- Page Heading -->
+        @isset($header)
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endisset
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+@endsection
